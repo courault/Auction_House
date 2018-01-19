9a0d4f367798c5f4b954a7208ce26acf8f03384b
courault <courault.f@gmail.com>
2018-01-19 15:23:34 +0100

    Transform Window into singleton

--- src/ah/ui/Window.java
+++ src/ah/ui/Window.java
@@ -8,12 +8,21 @@ public class Window extends JFrame {
 	private static final int W = 865;
 	private static final int H = 500;
 	
-	public Window() {
+	private static Window instance = null;	//Contient l'unique instance de la classe Window
+	
+	private Window() {
 		super(TITLE);
 		setSize(W, H);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setResizable(false);
 		setVisible(true);
+	}
+	
+	public final static Window getInstance() {
+		if (instance == null) {
+			instance = new Window();
+		}
+		return instance;
 	}
 }
