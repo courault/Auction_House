2c73afa809c7b94185f3eced593a62ee4c4db5bf
dedrouas <dedrouas@uqtr.ca>
2018-01-19 15:41:41 +0100

    Name & prive in Item

--- src/ah/Item.java
+++ src/ah/Item.java
@@ -1,5 +1,11 @@
 package ah;
 
 class Item {
+    private final String name;
+    private final int price;
     
+    public Item(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
 }
