c33895d88d6b229d34c8c4868c77acb3ed9e10e2
alexandrelorange <lorange@et.esiea.fr>
2018-01-22 11:16:28 +0100

    bid finish, test needed

--- src/ah/Bidder.java
+++ src/ah/Bidder.java
@@ -1,11 +1,15 @@
 package ah;
 
+import java.util.Random;
+
 public class Bidder implements Observer {
 
     private int wallet;
     public int ID;
 	private int commonPrice;
 	private int currentPrice;
+    public static Random rand = new Random();
+
 
     public int getID() {
         return ID;
@@ -27,31 +31,41 @@ public class Bidder implements Observer {
         wallet += bid;
     }
 
+	public void whoBid(){
+		try {
+			commonPrice = AuctionHouse.getListSellers().get(0).getCurrentItem().getCommonPrice();
+			currentPrice = AuctionHouse.getListSellers().get(0).getCurrentItem().getCommonPrice();
+			if(commonPrice > currentPrice) {
+				if( rand.nextInt(1000)>500) {
+					bid(this,currentPrice+20);
+				}
+			} else if (commonPrice*1.3 < currentPrice){
+				if( rand.nextInt(1000)>800) {
+					bid(this,currentPrice+10);
+				}
+			} else {
+				if( rand.nextInt(1000)>950) {
+					bid(this,currentPrice+5);
+				}
+			}
+
+
+		} catch ( EmptyItemListException e) {
+			System.out.println("List empty: end of sales");
+		}
+	}
+
     public Bidder(int wallet, int ID) {
         this.wallet = wallet;
         this.ID = ID;
         AuctionHouse.getListSellers().get(0).subscribe(this);
         //regarder si il y a un item si oui est ce que je bid ? puis appeler bid()
-		try {
-			commonPrice = AuctionHouse.getListSellers().get(0).getCurrentItem().getCommonPrice();
-			currentPrice = AuctionHouse.getListSellers().get(0).getCurrentItem().getCommonPrice();
-
-		} catch ( EmptyItemListException e) {
-			System.out.println("List empty: end of sales");
-		}
-
-
+		whoBid();
     }
 
     @Override
     public void refresh() {
-		try {
-			commonPrice = AuctionHouse.getListSellers().get(0).getCurrentItem().getCommonPrice();
-			currentPrice = AuctionHouse.getListSellers().get(0).getCurrentItem().getCommonPrice();
-
-		} catch ( EmptyItemListException e) {
-			System.out.println("List empty: end of sales");
-		}
+		whoBid();
     }
 
     @Override
